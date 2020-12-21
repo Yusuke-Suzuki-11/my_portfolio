3d4b9b8af8cf01ce56a095ae9705571b3500fca3
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2020-12-21 23:37:27 +0900

    トップページスキル入力

---
+++ resources/views/elements/header.blade.php
@@ -0,0 +1,16 @@
+<header class="main-header">
+	<div class="main-logo">
+		<a href="#" class="main-logo-link">鈴木佑輔<span style="font-size: 13px">です。</span></a>
+	</div>
+	<ul class="main-header-menus">
+		<li>
+		<a href='#' class="header-menus-li" >ポートフォリオ一覧</a>
+		</li>
+		<li>
+			<a href='#' class="header-menus-li">プロフィール</a>
+		</li>
+		<li>
+			<a href='#' class="header-menus-li">所属クリエイターチーム</a>
+		</li>
+	</ul>
+</header>
